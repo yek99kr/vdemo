61a0cb8ead4adfed082559c5c7718e7d48819ee8
yek99kr <55264386+yek99kr@users.noreply.github.com>
2021-11-16 01:53:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ document.addEventListener("click", function () {
   video1.volume = 0;
 });
 
+// document.body.appendChild(video1);
+
+// function processVideo() {
+//   videoctx.drawImage(video, 0, 0);
+// }
+
+// function draw() {
+//   ctx.fillRect(0, 0, canvas.width, canvas.height);
+// }
 function getNode(n, v) {
   n = document.createElementNS("http://www.w3.org/2000/svg", n);
   for (var p in v)
@@ -21,25 +30,75 @@ function getNode(n, v) {
   return n;
 }
 
-
+// requestAnimationFrame(draw);
 let x1, y1;
 var _svgNS = "http://www.w3.org/2000/svg";
 var parent = document.getElementById("myClip");
 
-function mouseMove(e) {
-  e.preventDefault();
+var isTouch = "ontouchstart" in document.documentElement;
 
-  const clientX = e.clientX || e.touches[0].clientX;
-  const clientY = e.clientY || e.touches[0].clientY;
-  var mask = getNode("rect", {
-    x: `${clientX - 30 + "px"}`,
-    y: `${clientY - 30 + "px"}`,
-    width: 60,
-    height: 60,
-  });
+//Check Device //All Touch Devices
+if (isTouch) {
+  function mouseMove(e) {
+    e.preventDefault();
+    //   var mask = document.createElementNS(_svgNS, "circle");
+    //   mask.setAttribute("cx", `${(e.pageX - 20 || e.clientX - 20) + "px"}`);
+    //   mask.setAttribute("cy", `${(e.pageY - 20 || e.clientY - 20) + "px"}`);
+    //   mask.setAttribute("fill", "blue");
+    const clientX = e.touches[0].clientX;
+    const clientY = e.touches[0].clientY;
+    var mask = getNode("rect", {
+      x: `${clientX - 30 + "px"}`,
+      y: `${clientY - 30 + "px"}`,
+      width: 60,
+      height: 60,
+    });
 
-  parent.appendChild(mask);
+    //   mask.style.top = (e.pageY - 20 || e.clientY - 20) + "px";
+    //   mask.style.left = (e.pageX - 20 || e.clientX - 20) + "px";
+    parent.appendChild(mask);
+  }
+} else {
+  function mouseMove(e) {
+    e.preventDefault();
+    //   var mask = document.createElementNS(_svgNS, "circle");
+    //   mask.setAttribute("cx", `${(e.pageX - 20 || e.clientX - 20) + "px"}`);
+    //   mask.setAttribute("cy", `${(e.pageY - 20 || e.clientY - 20) + "px"}`);
+    //   mask.setAttribute("fill", "blue");
+    const clientX = e.clientX;
+    const clientY = e.clientY;
+    var mask = getNode("rect", {
+      x: `${clientX - 30 + "px"}`,
+      y: `${clientY - 30 + "px"}`,
+      width: 60,
+      height: 60,
+    });
+
+    //   mask.style.top = (e.pageY - 20 || e.clientY - 20) + "px";
+    //   mask.style.left = (e.pageX - 20 || e.clientX - 20) + "px";
+    parent.appendChild(mask);
+  }
 }
+
+// function mouseMove(e) {
+//   e.preventDefault();
+//   //   var mask = document.createElementNS(_svgNS, "circle");
+//   //   mask.setAttribute("cx", `${(e.pageX - 20 || e.clientX - 20) + "px"}`);
+//   //   mask.setAttribute("cy", `${(e.pageY - 20 || e.clientY - 20) + "px"}`);
+//   //   mask.setAttribute("fill", "blue");
+//   const clientX = e.clientX || e.touches[0].clientX;
+//   const clientY = e.clientY || e.touches[0].clientY;
+//   var mask = getNode("rect", {
+//     x: `${clientX - 30 + "px"}`,
+//     y: `${clientY - 30 + "px"}`,
+//     width: 60,
+//     height: 60,
+//   });
+
+//   //   mask.style.top = (e.pageY - 20 || e.clientY - 20) + "px";
+//   //   mask.style.left = (e.pageX - 20 || e.clientX - 20) + "px";
+//   parent.appendChild(mask);
+// }
 
 document.addEventListener("mousemove", mouseMove);
 document.addEventListener("touchmove", mouseMove);
